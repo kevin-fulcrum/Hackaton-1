8d67297b9c3053e5e192117e63262d32af26541e
Kevin <kevinariassistemas@gmail.com>
2020-10-10 09:38:47 -0500

    Ejercicio 4

---
+++ Ejercicio4.js
@@ -0,0 +1,10 @@
+/*
+TODO: Ejercicio #4: Creando arrays
+TODO: - Crear un array llamado *meses* y que almacene el nombre de los doce meses del año
+TODO: - Mostrar por pantalla los doce nombres utilizando la función *alert()*
+*/
+
+const Months = new Array ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre');
+for (let i of Months){
+    console.log("Mes: ", i)
+}
