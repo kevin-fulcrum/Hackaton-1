ae3d9a931a783351c4639a9c0a9e2df68cce0b9a
Kevin <kevinariassistemas@gmail.com>
2020-10-10 08:37:23 -0500

    Ejercicio 0

--- Ejercicio0.js
+++ Ejercicio0.js
@@ -2,5 +2,5 @@ let Numeros = [6, 15, 8, 2, 45, 68, 3, 24]
 const SortNumericallu = (a,b) => {
     return a - b;
 }
-NumerosAsc = Numeros.sort(SortNumericallu)
+Numeros.sort(SortNumericallu)
 console.log("El menor numero es: " + Numeros[0]);
