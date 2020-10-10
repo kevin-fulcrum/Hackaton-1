2419b95b828d256d74fc437eeba0d0329bb1c262
Kevin <kevinariassistemas@gmail.com>
2020-10-10 09:25:34 -0500

    Ejercicio 1

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,3 +1,13 @@
+/*
+TODO: Ejercicio #1: Información Factura
+TODO: - Definir la estructura de un objeto que almacena una factura
+TODO: - Las facturas están formadas por la información de la propia empresa (nombre de la empresa, dirección, teléfono, NIF), la información del cliente
+TODO: (similar a la de la empresa), una lista de elementos (cada uno de los cuales dispone de descripción, precio, cantidad) y otra información básica de la
+TODO: factura (importe total, tipo de IVA, forma de pago)
+TODO: - Una vez definidas las propiedades del objeto, añadir un método que calcule el importe total de la factura y actualice el valor de la propiedad
+TODO: correspondiente. Por último, añadir otro método que muestre por pantalla el importe total de la factura
+*/
+
 function Factura(empresa, cliente, producto, factura) {
 	this.empresa = empresa;
 	this.cliente = cliente;
